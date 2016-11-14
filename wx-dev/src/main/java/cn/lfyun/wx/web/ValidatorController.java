0aff55c6d5077cf958ba4fecc58e64a00c54c16d
廖鹏 <liaopeng@meizu.com>
2016-11-14 15:22:46 +0800

    d

--- wx-dev/src/main/java/cn/lfyun/wx/web/ValidatorController.java
+++ wx-dev/src/main/java/cn/lfyun/wx/web/ValidatorController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,15 @@ import cn.lfyun.wx.form.LoginForm;
 import cn.lfyun.wx.form.UserBean;
 import cn.lfyun.wx.service.RandomBean;
 
+import com.nettyrpc.test.client.HelloService;
+
 @Controller
 public class ValidatorController {
 
 	@Resource
 	private RandomBean randomBean;
+	@Autowired
+	private HelloService helloService;
 	
 	private AtomicInteger count = new AtomicInteger(0);
 	@RequestMapping("/login")
@@ -45,6 +50,14 @@ public class ValidatorController {
 		long start = System.currentTimeMillis();
 		String content = randomBean.random();
 		System.out.println("count=" + c + " cost " + (System.currentTimeMillis() - start) + "ms " + content);
+		return Message.newBuilder().data(content).build();
+	}
+	@RequestMapping("/hello")
+	public @ResponseBody
+	Object hello() {
+		long start = System.currentTimeMillis();
+		String content = helloService.hello("廖鹏");
+		System.out.println("invoke cost " + (System.currentTimeMillis() - start) + "ms " + content);
 		return Message.newBuilder().data(content).build();
 	}
 }

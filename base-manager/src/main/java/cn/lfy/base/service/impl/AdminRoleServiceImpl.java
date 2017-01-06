e9434fa3df22aabf16ad9b7a5ed2969e3eeb6969
廖鹏 <liaopeng@meizu.com>
2017-01-06 13:37:07 +0800

    角色权限控制

--- base-manager/src/main/java/cn/lfy/base/service/impl/AdminRoleServiceImpl.java
+++ base-manager/src/main/java/cn/lfy/base/service/impl/AdminRoleServiceImpl.java
@@ -1,12 +1,11 @@
 package cn.lfy.base.service.impl;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.google.common.collect.Sets;
 
 import cn.lfy.base.dao.AdminRoleDAO;
 import cn.lfy.base.dao.RoleDAO;
@@ -15,6 +14,9 @@ import cn.lfy.base.model.Role;
 import cn.lfy.base.service.AdminRoleService;
 import cn.lfy.common.framework.exception.ApplicationException;
 import cn.lfy.common.framework.exception.ErrorCode;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 @Service
 public class AdminRoleServiceImpl implements AdminRoleService
@@ -53,20 +55,57 @@ public class AdminRoleServiceImpl implements AdminRoleService
     	if(roles.size() != roleIds.size()) {
     		throw ApplicationException.newInstance(ErrorCode.PARAM_ILLEGAL, "roleIds");
     	}
-    	Set<Long> nowRoleIds = Sets.newHashSet(roleIds);
-    	List<Role> curUserRoles = getRoleListByAdminId(userId);
-    	Set<Long> curUserRolesSet = Sets.newHashSet();
-    	for(Role role : curUserRoles) {
-    		curUserRolesSet.add(role.getId());
+    	Map<Long, Role> userRoleMap = Maps.newHashMap();
+    	//有效的角色ID
+    	Set<Long> nowRoleIds = Sets.newHashSet();
+    	for(Role role : roles) {
+    		nowRoleIds.add(role.getId());
+    		userRoleMap.put(role.getId(), role);
     	}
-    	Set<Long> delSet = Sets.difference(curUserRolesSet, nowRoleIds);
-		Set<Long> newSet = Sets.difference(nowRoleIds, curUserRolesSet);
+    	List<Role> userRoles = getRoleListByAdminId(userId);
+    	Set<Long> userRolesSet = Sets.newHashSet();
+    	
+    	for(Role role : userRoles) {
+    		userRolesSet.add(role.getId());
+    		userRoleMap.put(role.getId(), role);
+    	}
+    	Set<Long> delSet = Sets.difference(userRolesSet, nowRoleIds);
+		Set<Long> newSet = Sets.difference(nowRoleIds, userRolesSet);
+		for(Long delRoleId : delSet) {
+			Role role = userRoleMap.get(delRoleId);
+			//判断操作者的用户的角色ID是否在该角色的父路径里
+			if(!isCurrentUserRolesOfChild(role, currentUserRoles)) {
+				throw ApplicationException.newInstance(ErrorCode.PERMISSION_DENIED);
+			}
+		}
 		for(Long delRoleId : delSet) {
 			adminRoleDAO.delete(userId, delRoleId);
 		}
 		for(Long roleId : newSet) {
+			Role role = userRoleMap.get(roleId);
+			if(!isCurrentUserRolesOfChild(role, currentUserRoles)) {
+				throw ApplicationException.newInstance(ErrorCode.PERMISSION_DENIED);
+			}
+		}
+		for(Long roleId : newSet) {
 			this.add(userId, roleId);
 		}
+    }
+    
+    /**
+     * 判断role是否是currentUserRoles（当前用户角色集合）的子孙
+     * @param role
+     * @param currentUserRoles
+     * @return
+     */
+    private boolean isCurrentUserRolesOfChild(Role role, Set<Role> currentUserRoles) {
+    	for(Role crole : currentUserRoles) {
+    		int idx = role.getParentIdPath().indexOf(crole.getParentIdPath() + crole.getId() + "$");
+    		if(idx >= 0) {
+    			return true;
+    		}
+    	}
+    	return false;
     }
 
 }
